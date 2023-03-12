4ebc4ff88d221fd4463842d9db4680841380afa8
Naomi Cama <naomi.cama@vallegrande.edu.pe>
2023-03-11 23:07:12 -0500

    Taller01

---
+++ Taller-01-Microservicios/src/main/java/com/msproduct/msproduct/service/CategoriaService.java
@@ -0,0 +1,15 @@
+package com.msproduct.msproduct.service;
+
+import com.msproduct.msproduct.model.Categoria;
+
+import java.util.List;
+
+public interface CategoriaService {
+    Categoria crear(Categoria categoria);
+
+    Categoria actualizar(Long id, Categoria categoria);
+
+    List<Categoria> listarTodos();
+
+    void eliminar(Long id);
+}
